e6a8cd255e44e1782274e74f7c5b2dea48e0fcc4
hwen <mhqnwt@gmail.com>
2021-05-08 10:45:07 +0800

    iOS
    Signed-off-by: hwen <mhqnwt@gmail.com>

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
 	name: "SwiftkubeClient",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v10_15),
+        .iOS(.v10),
 	],
 	products: [
 		.library(
